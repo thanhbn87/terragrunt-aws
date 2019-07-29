cfd970825a01402cd4df4f3989eaef29a0f2af34
thanhbn <thanhbn87@gmail.com>
2019-07-29 15:10:56 +0700

    vpc: add s3 endpoint

--- vpc/main.tf
+++ vpc/main.tf
@@ -55,6 +55,15 @@ module "vpc" {
 }
 
 ///////////////////////////////////
+//           Endpoint            //
+///////////////////////////////////
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id          = "${module.vpc.vpc_id}"
+  service_name    = "com.amazonaws.${var.aws_region}.s3"
+  route_table_ids = ["${concat(module.vpc.private_route_table_ids,module.vpc.public_route_table_ids)}"]
+}
+
+///////////////////////////////////
 //            Route53            //
 ///////////////////////////////////
 resource "aws_route53_zone" "private" {
