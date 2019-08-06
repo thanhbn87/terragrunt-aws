f02155b841339dca75f9f21bd557165c0cbad92f
thanhbn <thanhbn87@gmail.com>
2019-08-06 10:54:34 +0700

    add database-v2

---
+++ security-group/private/database-v2/main.tf
@@ -0,0 +1,61 @@
+provider "aws" {
+  region  = "${var.aws_region}"
+  profile = "${var.aws_profile}"
+}
+
+terraform {
+  backend "s3" {}
+}
+
+data "aws_vpc" "vpc" {
+  tags {
+    Name = "${var.vpc_name}"
+  }
+}
+
+data "aws_security_groups" "source_sg" {
+  tags = "${var.source_sg_tags}"
+  
+  filter {
+    name   = "vpc-id"
+    values = ["${data.aws_vpc.vpc.id}"]
+  }
+}
+
+resource "null_resource" "ingress_with_source_sgs" {
+  count = "${length(data.aws_security_groups.source_sg.ids)}"
+
+  triggers {
+    rule                     = "db-port"
+    description              = "${element(data.aws_security_groups.source_sg.ids, count.index)}"
+    source_security_group_id = "${element(data.aws_security_groups.source_sg.ids, count.index)}"
+  }
+}
+
+locals {
+  ingress_with_source_sgs = "${null_resource.ingress_with_source_sgs.*.triggers}"
+}
+
+module "security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "2.17.0"
+
+  use_name_prefix = false
+  name            = "${lower(var.project_env_short)}-${lower(var.name)}"
+  description     = "${var.description}"
+  vpc_id          = "${data.aws_vpc.vpc.id}"
+
+  rules                         = "${var.rules}"
+  ingress_with_cidr_blocks      = "${var.ingress_with_cidr_blocks}"
+  ingress_with_ipv6_cidr_blocks = "${var.ingress_with_ipv6_cidr_blocks}"
+  ingress_with_self             = "${var.ingress_with_self}"
+  ingress_with_source_security_group_id = "${local.ingress_with_source_sgs}"
+
+  egress_cidr_blocks            = "${var.egress_cidr_blocks}"
+  egress_ipv6_cidr_blocks       = "${var.egress_ipv6_cidr_blocks}"
+  egress_rules                  = "${var.egress_rules}"
+  egress_with_cidr_blocks       = "${var.egress_with_cidr_blocks}"
+  egress_with_ipv6_cidr_blocks  = "${var.egress_with_ipv6_cidr_blocks}"
+
+  tags = "${merge(var.tags, map("Env", "${var.project_env}"))}"
+}
---
+++ security-group/private/database-v2/vars.tf
@@ -0,0 +1,45 @@
+variable "project_name" { default = "Demo" }
+variable "project_env" { default = "Production" }
+variable "project_env_short" { default = "prd" }
+
+variable "aws_region" { default = "us-west-2" }
+variable "aws_profile" { default = "default" }
+
+variable "name" { default = "security-group" }
+variable "description" { default = "This is a security group for Database access" }
+variable "vpc_name" { default = "default" }
+
+variable "source_sg_tags" { default = { AccessDB = "1" } }
+
+variable "ingress_with_cidr_blocks" { default = [] }
+variable "ingress_with_ipv6_cidr_blocks" { default = [] }
+variable "ingress_with_self" { 
+default = [
+  {
+    from_port   = "-1"
+    to_port     = "-1"
+    protocol    = "-1"
+    description = "Itself"
+  }
+  ]
+}
+
+variable "egress_cidr_blocks" { default = ["0.0.0.0/0"] }
+variable "egress_ipv6_cidr_blocks" { default = ["::/0"] }
+variable "egress_rules" { default = ["all-all"] }
+variable "egress_with_cidr_blocks" { default = [] }
+variable "egress_with_ipv6_cidr_blocks" { default = [] }
+
+variable "rules" {
+  description = "Map of known security group rules (define as 'name' = ['from port', 'to port', 'protocol', 'description'])"
+  type        = "map"
+  default = {
+    db-port = [3306, 3306, "tcp", "MySQL/Aurora"]
+    all-all = [-1, -1, "-1", "All protocols"]
+    _       = ["", "", ""]
+  }
+}
+
+variable "tags" {
+  default = {}
+}
