e515bf0d25590ad80cbf3a1dd1004c90ea1d22cf
thanhbn <thanhbn87@gmail.com>
2019-08-12 15:34:15 +0700

    add namespace

--- security-group/private/common/vars.tf
+++ security-group/private/common/vars.tf
@@ -5,6 +5,7 @@ variable "project_env_short" { default = "prd" }
 variable "aws_region" { default = "us-west-2" }
 variable "aws_profile" { default = "default" }
 
+variable "namespace" { default = "" }
 variable "name" { default = "security-group" }
 variable "description" { default = "This is a common security group" }
 variable "vpc_name" { default = "default" }
