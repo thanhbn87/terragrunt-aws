267b9fdc0a0999d27c1746b2efa1645656c8fc28
thanhbn <thanhbn87@gmail.com>
2019-08-17 13:38:11 +0700

    fix multi subs

--- acm/validation_cloudflare/main.tf
+++ acm/validation_cloudflare/main.tf
@@ -33,7 +33,7 @@ resource "aws_acm_certificate" "cert" {
 }
 
 resource "cloudflare_record" "cert_validation" {
-  count  = "${var.cloudflare_record ? "${length(var.subject_alternative_names) == 0 ? 1 : length(var.subject_alternative_names)}" : 0}"
+  count  = "${var.cloudflare_record ? "${length(var.subject_alternative_names) == 0 ? 1 : length(var.subject_alternative_names)+1 }" : 0}"
   domain = "${var.root_domain}"
   name   = "${lookup(aws_acm_certificate.cert.domain_validation_options[count.index],"resource_record_name")}"
   value  = "${lookup(aws_acm_certificate.cert.domain_validation_options[count.index],"resource_record_value")}"
